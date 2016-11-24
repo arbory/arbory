3b815dd1c7657f8847d9531cc46810d175abbded
Krists Krigers <krists.krigers@gmail.com>
2016-11-24 17:13:15 +0200

    Add leaf files table migration

--- database/migrations/2016_11_24_133000_create_leaf_files_table.php
+++ database/migrations/2016_11_24_133000_create_leaf_files_table.php
@@ -1,10 +1,7 @@
 <?php
 
-namespace Leaf\database\migrations;
-
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Schema;
 
 /**
  * Class CreateLeafFilesTable
