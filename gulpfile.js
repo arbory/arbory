0478ae0b445088483db9bf0783527940acc3d298
Krists Krigers <krists.krigers@gmail.com>
2016-11-24 17:01:53 +0200

    Fix dependency issues

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,64 @@
-const elixir = require('laravel-elixir');
+module.exports = function(elixir){
 
-elixir(mix => {
+    elixir(mix => {
 
-    mix.scriptsIn(
-        'packages/CubeSystems/Leaf/resources/assets/javascripts/include/',
-        'public/leaf/js/dependencies.min.js'
-    );
+        mix.scriptsIn(
+            'packages/CubeSystems/Leaf/resources/assets/javascripts/include/',
+            'public/leaf/js/dependencies.min.js'
+        );
 
-    mix.scripts(
-        [
-            'jquery/jquery.min.js',
-            'jqueryui/jquery-ui.min.js',
-            'jquery-cookie/jquery.cookie.js',
-            '../ckeditor/ckeditor/ckeditor.js',
-            '../ckeditor/ckeditor/adapters/jquery.js',
-        ],
-        'public/leaf/js/vendor.min.js',
-        'packages/CubeSystems/Leaf/vendor/components/'
-    );
+        mix.scripts(
+            [
+                'jquery/jquery.min.js',
+                'jqueryui/jquery-ui.min.js',
+                'jquery-cookie/jquery.cookie.js',
+                '../ckeditor/ckeditor/ckeditor.js',
+                '../ckeditor/ckeditor/adapters/jquery.js',
+            ],
+            'public/leaf/js/vendor.min.js',
+            'packages/CubeSystems/Leaf/vendor/components/'
+        );
 
-    mix.scripts([
-            'vendor.min.js',
-            'dependencies.min.js',
-        ],
-        'public/leaf/js/application.js',
-        'public/leaf/js/'
-    );
+        mix.scripts([
+                'vendor.min.js',
+                'dependencies.min.js',
+            ],
+            'public/leaf/js/application.js',
+            'public/leaf/js/'
+        );
 
-    mix.copy(
-        'packages/CubeSystems/Leaf/resources/assets/javascripts/controllers/',
-        'public/leaf/js/controllers/'
-    );
+        mix.copy(
+            'packages/CubeSystems/Leaf/resources/assets/javascripts/controllers/',
+            'public/leaf/js/controllers/'
+        );
 
-    mix.sass(
-        'application.scss',
-        'public/leaf/css/application.css',
-        'packages/CubeSystems/Leaf/resources/assets/stylesheets/'
-    );
+        mix.sass(
+            'application.scss',
+            'public/leaf/css/application.css',
+            'packages/CubeSystems/Leaf/resources/assets/stylesheets/'
+        );
 
-    mix.sass(
-        'controllers/nodes.scss',
-        'public/leaf/css/controllers/',
-        'packages/CubeSystems/Leaf/resources/assets/stylesheets/'
-    );
+        mix.sass(
+            'controllers/nodes.scss',
+            'public/leaf/css/controllers/',
+            'packages/CubeSystems/Leaf/resources/assets/stylesheets/'
+        );
 
-    mix.sass(
-        'controllers/sessions.scss',
-        'public/leaf/css/controllers/',
-        'packages/CubeSystems/Leaf/resources/assets/stylesheets/'
-    );
+        mix.sass(
+            'controllers/sessions.scss',
+            'public/leaf/css/controllers/',
+            'packages/CubeSystems/Leaf/resources/assets/stylesheets/'
+        );
 
-    mix.copy(
-        'packages/CubeSystems/Leaf/vendor/ckeditor/ckeditor/',
-        'public/leaf/ckeditor/'
-    );
+        mix.copy(
+            'packages/CubeSystems/Leaf/vendor/ckeditor/ckeditor/',
+            'public/leaf/ckeditor/'
+        );
 
-    mix.copy(
-        'packages/CubeSystems/Leaf/resources/assets/images/',
-        'public/leaf/images/'
-    );
+        mix.copy(
+            'packages/CubeSystems/Leaf/resources/assets/images/',
+            'public/leaf/images/'
+        );
 
-});
+    });
+};
