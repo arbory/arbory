0d4905a27a3579a6c3074df4cf31de6a1655c8f8
Roberts <roberts@cubesystems.lv>
2016-11-24 20:56:18 +0200

    Extend text field to avoid duplication

--- src/Fields/DateTime.php
+++ src/Fields/DateTime.php
@@ -2,33 +2,7 @@
 
 namespace CubeSystems\Leaf\Fields;
 
-class DateTime extends AbstractField
+class DateTime extends Text
 {
-    /**
-     * @param array $attributes
-     * @return \Illuminate\View\View
-     */
-    public function render( array $attributes = [] )
-    {
-        if( $this->isForList() )
-        {
-            $model = $this->getModel();
 
-            return view( $this->getViewName(), [
-                'field' => $this,
-                'attributes' => $attributes,
-                'url' => route( 'admin.model.edit', [
-                    $this->getFieldSet()->getController()->getSlug(),
-                    $model->{$model->getKeyName()}
-                ] ),
-            ] );
-        }
-        elseif( $this->isForForm() )
-        {
-            return view( $this->getViewName(), [
-                'field' => $this,
-                'attributes' => $attributes,
-            ] );
-        }
-    }
 }
