44873849360ae643081c48ac19402b230eb47d55
Roberts <roberts@cubesystems.lv>
2016-12-12 10:31:46 +0200

    Update default Leaf config

--- config/leaf.php
+++ config/leaf.php
@@ -7,15 +7,24 @@ return [
             'title' => 'Nodes',
             'controller' => \CubeSystems\Leaf\Http\Controllers\Admin\NodeController::class,
         ],
+        [
+            'title' => 'Users',
+            'items' =>[
+                [
+                    'title' => 'Admin users',
+                    'route' => 'admin.users.index',
+                    'roles' => [ 'users_admin' ]
+                ],
+                [
+                    'title' => 'Admin roles',
+                    'route' => 'admin.roles.index',
+                    'roles' => [ 'roles_admin' ]
+                ],
+            ]
+        ],
     ],
     'pagination' => [
         'items_per_page' => 15,
-    ],
-    'field_types' => [
-        'belongsTo' => \CubeSystems\Leaf\Fields\BelongsTo::class,
-        'richtext' => \CubeSystems\Leaf\Fields\Richtext::class,
-        'text' => \CubeSystems\Leaf\Fields\Text::class,
-        'toolbox' => \CubeSystems\Leaf\Fields\Toolbox::class,
     ],
     'content_types' => [
         \CubeSystems\Leaf\Pages\TextPage::class,
