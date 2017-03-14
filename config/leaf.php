3426e972093b4b82e5f4e489f016ab8ba03cd9db
Roberts <roberts@cubesystems.lv>
2017-03-15 01:14:05 +0200

    [WIP] Fix default configuration file

--- config/leaf.php
+++ config/leaf.php
@@ -8,8 +8,9 @@ return [
     'menu' => [
         [
             'title' => 'Nodes',
-            'type' => AbstractItem::TYPE_CRUD_MODULE,
+            'type' => AbstractItem::TYPE_MODULE,
             'module_name' => 'nodes',
+            'route_name' => 'admin.nodes.index'
         ],
         [
             'title' => 'Users',
@@ -19,13 +20,13 @@ return [
                     'title' => 'Admin users',
                     'type' => AbstractItem::TYPE_MODULE,
                     'module_name' => 'admin_users',
-                    'route_name' => 'admin.users.index',
+                    'route_name' => 'admin.users.index'
                 ],
                 [
                     'title' => 'Admin roles',
                     'type' => AbstractItem::TYPE_MODULE,
                     'module_name' => 'admin_roles',
-                    'route_name' => 'admin.roles.index',
+                    'route_name' => 'admin.roles.index'
                 ],
             ]
         ],
@@ -37,20 +38,27 @@ return [
         ],
         [
             'name' => 'nodes',
-            'controller_class' => \CubeSystems\Leaf\Http\Controllers\Admin\NodeCrudController::class,
+            'controller_class' => \CubeSystems\Leaf\Http\Controllers\Admin\NodesController::class,
         ],
         [
             'name' => 'admin_users',
-            'controller_class' => \CubeSystems\Leaf\Http\Controllers\Admin\UserController::class,
+            'controller_class' => \CubeSystems\Leaf\Http\Controllers\Admin\UsersController::class,
             'authorization_type' => Module::AUTHORIZATION_TYPE_ROLES,
             'authorized_roles' => [ 'administrator' ],
         ],
         [
             'name' => 'admin_roles',
-            'controller_class' => \CubeSystems\Leaf\Http\Controllers\Admin\RoleController::class,
+            'controller_class' => \CubeSystems\Leaf\Http\Controllers\Admin\RolesController::class,
             'authorization_type' => Module::AUTHORIZATION_TYPE_ROLES,
             'authorized_roles' => [ 'administrator' ],
         ],
+        [
+            'name' => 'translations',
+            'controller_class' => \CubeSystems\Leaf\Http\Controllers\Admin\TranslationsController::class,
+            'authorization_type' => Module::AUTHORIZATION_TYPE_ROLES,
+            'authorized_roles' => [ 'administrator' ],
+        ],
+
     ],
     'pagination' => [
         'items_per_page' => 15,
