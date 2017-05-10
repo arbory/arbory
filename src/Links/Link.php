3f681a5cca360d6abea91aac7a323207e76280c1
Niklavs Strads <strads4@live.com>
2017-05-10 09:26:34 +0300

    Add toString to link

--- src/Links/Link.php
+++ src/Links/Link.php
@@ -6,9 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Link extends Model
 {
+    /**
+     * @var array
+     */
     protected $fillable = [
         'title',
         'new_tab',
         'href',
     ];
+
+    /**
+     * @return string
+     */
+    public function __toString()
+    {
+        return (string) $this->href;
+    }
 }
