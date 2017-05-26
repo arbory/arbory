bfb9c11a0706b367c579e75b8d1461ebe92f36b8
Niklavs Strads <niklavs.strads@cubesystems.lv>
2017-05-26 11:52:42 +0300

    Register modules with middleware

--- routes/admin.php
+++ routes/admin.php
@@ -1,17 +1,17 @@
 <?php
 
-AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\MenuBuilderController::class );
-AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\NodesController::class );
-AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\RolesController::class );
-AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\UsersController::class );
-AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\SettingsController::class );
-
 Route::get( '/', [ 'as' => 'login.form', 'uses' => 'Admin\SessionController@getLogin' ] );
 Route::post( 'login', [ 'as' => 'login.attempt', 'uses' => 'Admin\SessionController@postLogin' ] );
 Route::post( 'logout', [ 'as' => 'logout', 'uses' => 'Admin\SessionController@postLogout' ] );
 
 Route::group( [ 'middleware' => 'leaf.admin_auth' ], function ()
 {
+    AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\MenuBuilderController::class );
+    AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\NodesController::class );
+    AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\RolesController::class );
+    AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\UsersController::class );
+    AdminModule::register( CubeSystems\Leaf\Http\Controllers\Admin\SettingsController::class );
+
     Route::get( 'dashboard', [
         'as' => 'dashboard',
         'uses' => 'Admin\DashboardController@index'
