752825b48fb472b9752a6209ecc1619c3741e6f7
Niklavs Strads <niklavs.strads@cubesystems.lv>
2017-06-28 10:57:15 +0300

    Mix admin assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,63 +1,52 @@
+let fs = require('fs-extra');
+
+const controllers = fs.readdirSync('vendor/cubesystems/leaf/resources/assets/javascripts/controllers');
+
 module.exports = function (mix) {
 
-    mix.combine(
-        './vendor/cubesystems/leaf/resources/assets/javascripts/include/**/*.js',
-        './public/leaf/js/dependencies.min.js'
+    mix.js(
+        './vendor/cubesystems/leaf/resources/assets/javascripts/admin.js',
+        'public/leaf/js'
     );
 
-    mix.scripts(
-        [
+    for (let name of controllers) {
+        mix.js('vendor/cubesystems/leaf/resources/assets/javascripts/controllers/' + name, 'public/leaf/js/controllers/');
+    }
+
+    mix.scripts([
+            './vendor/cubesystems/leaf/resources/assets/javascripts/environment.js',
             './vendor/components/jquery/jquery.min.js',
             './vendor/components/jqueryui/jquery-ui.min.js',
             './vendor/components/jquery-cookie/jquery.cookie.js',
             './vendor/ckeditor/ckeditor/ckeditor.js',
             './vendor/ckeditor/ckeditor/adapters/jquery.js',
+            './vendor/cubesystems/leaf/resources/assets/javascripts/include/**/*.js',
         ],
-        './public/leaf/js/vendor.min.js'
-    );
-
-    mix.js(
-        './vendor/cubesystems/leaf/resources/assets/javascripts/admin.js',
-        './public/leaf/js'
-    );
-
-    mix.scripts([
-            './vendor/cubesystems/leaf/resources/assets/javascripts/environment.js',
-            './public/leaf/js/vendor.min.js',
-            './public/leaf/js/dependencies.min.js',
-            './public/leaf/js/admin.js'
-        ],
-        './public/leaf/js/application.js'
-    );
-
-    mix.copyDirectory(
-        './vendor/cubesystems/leaf/resources/assets/javascripts/controllers/',
-        './public/leaf/js/controllers/'
+        'public/leaf/js/application.js'
     );
 
     mix.sass(
-        './vendor/cubesystems/leaf/resources/assets/stylesheets/application.scss',
-        './public/leaf/css/application.css'
+        'vendor/cubesystems/leaf/resources/assets/stylesheets/application.scss',
+        'public/leaf/css/application.css'
     );
 
     mix.sass(
-        './vendor/cubesystems/leaf/resources/assets/stylesheets/controllers/nodes.scss',
-        './public/leaf/css/controllers/'
+        'vendor/cubesystems/leaf/resources/assets/stylesheets/controllers/nodes.scss',
+        'public/leaf/css/controllers/'
     );
 
     mix.sass(
-        './vendor/cubesystems/leaf/resources/assets/stylesheets/controllers/sessions.scss',
-        './public/leaf/css/controllers/'
+        'vendor/cubesystems/leaf/resources/assets/stylesheets/controllers/sessions.scss',
+        'public/leaf/css/controllers/'
     );
 
     mix.copyDirectory(
-        './vendor/ckeditor/ckeditor/',
-        './public/leaf/ckeditor/'
+        'vendor/ckeditor/ckeditor/',
+        'public/leaf/ckeditor/'
     );
 
     mix.copyDirectory(
-        './vendor/cubesystems/leaf/resources/assets/images/',
-        './public/leaf/images/'
+        'vendor/cubesystems/leaf/resources/assets/images/',
+        'public/leaf/images/'
     );
-
 };
