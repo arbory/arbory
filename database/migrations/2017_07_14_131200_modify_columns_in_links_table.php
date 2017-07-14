583ac165379791515e9b610d2b8e44db429ccf3c
Niklavs Strads <niklavs.strads@cubesystems.lv>
2017-07-14 14:04:38 +0300

    Allow empty links

---
+++ database/migrations/2017_07_14_131200_modify_columns_in_links_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class ModifyColumnsInLinksTable extends Migration
+{
+    /**
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table( 'links', function( Blueprint $table )
+        {
+            $table->string( 'title' )->nullable()->change();
+            $table->text( 'href' )->nullable()->change();
+        } );
+    }
+}
