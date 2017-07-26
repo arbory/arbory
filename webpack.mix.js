b1ebec2038bc2a390e1ba46abba0d7acba377879
Niklavs Strads <niklavs.strads@cubesystems.lv>
2017-07-26 11:25:29 +0300

    Fix asset paths

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,42 +1,42 @@
 let fs = require('fs-extra');
 
-const controllers = fs.readdirSync('vendor/arbory/base/resources/assets/js/controllers');
+const controllers = fs.readdirSync('vendor/arbory/arbory/resources/assets/js/controllers');
 
 module.exports = function (mix) {
 
     mix.js(
-        './vendor/arbory/base/resources/assets/js/admin.js',
+        './vendor/arbory/arbory/resources/assets/js/admin.js',
         'public/arbory/js'
     );
 
     for (let name of controllers) {
-        mix.js('vendor/arbory/base/resources/assets/js/controllers/' + name, 'public/arbory/js/controllers/');
+        mix.js('vendor/arbory/arbory/resources/assets/js/controllers/' + name, 'public/arbory/js/controllers/');
     }
 
     mix.scripts([
-            './vendor/arbory/base/resources/assets/js/environment.js',
+            './vendor/arbory/arbory/resources/assets/js/environment.js',
             './vendor/components/jquery/jquery.min.js',
             './vendor/components/jqueryui/jquery-ui.min.js',
             './vendor/components/jquery-cookie/jquery.cookie.js',
             './vendor/ckeditor/ckeditor/ckeditor.js',
             './vendor/ckeditor/ckeditor/adapters/jquery.js',
-            './vendor/arbory/base/resources/assets/js/include/**/*.js',
+            './vendor/arbory/arbory/resources/assets/js/include/**/*.js',
         ],
         'public/arbory/js/application.js'
     );
 
     mix.sass(
-        'vendor/arbory/base/resources/assets/stylesheets/application.scss',
+        'vendor/arbory/arbory/resources/assets/stylesheets/application.scss',
         'public/arbory/css/application.css'
     );
 
     mix.sass(
-        'vendor/arbory/base/resources/assets/stylesheets/controllers/nodes.scss',
+        'vendor/arbory/arbory/resources/assets/stylesheets/controllers/nodes.scss',
         'public/arbory/css/controllers/'
     );
 
     mix.sass(
-        'vendor/arbory/base/resources/assets/stylesheets/controllers/sessions.scss',
+        'vendor/arbory/arbory/resources/assets/stylesheets/controllers/sessions.scss',
         'public/arbory/css/controllers/'
     );
 
@@ -46,12 +46,12 @@ module.exports = function (mix) {
     );
 
     mix.copyDirectory(
-        'vendor/arbory/base/resources/assets/js/lib/ckeditor/plugins/',
+        'vendor/arbory/arbory/resources/assets/js/lib/ckeditor/plugins/',
         'public/arbory/ckeditor/plugins/'
     );
 
     mix.copyDirectory(
-        'vendor/arbory/base/resources/assets/images/',
+        'vendor/arbory/arbory/resources/assets/images/',
         'public/arbory/images/'
     );
 };
