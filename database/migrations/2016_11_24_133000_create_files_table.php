63e1b898a0ca8e3eb9d9f60639694b77b5620ed3
Roberts <roberts@cubesystems.lv>
2017-07-27 19:40:04 +0300

    Fix migration class name

--- database/migrations/2016_11_24_133000_create_files_table.php
+++ database/migrations/2016_11_24_133000_create_files_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
-class CreateArboryFilesTable extends Migration
+class CreateFilesTable extends Migration
 {
     /**
      *
