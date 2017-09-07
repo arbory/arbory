6d8ed0f2ef7915dcaa6550fa8cb23e45dd130f19
Niklavs Strads <niklavs.strads@cubesystems.lv>
2017-09-07 12:16:15 +0300

    Add TextPage to pagez stub

--- src/Pages/TextPage.php
+++ src/Pages/TextPage.php
@@ -2,36 +2,19 @@
 
 namespace Arbory\Base\Pages;
 
-use Arbory\Base\Admin\Form\FieldSet;
-use Arbory\Base\Admin\Form\Fields\Richtext;
-use Arbory\Base\Html\Elements\Element;
 use Illuminate\Database\Eloquent\Model;
 
-/**
- * Class TextPage
- * @package Arbory\Base\Pages
- */
-class TextPage extends Model implements PageInterface
+class TextPage extends Model
 {
+    /**
+     * @var string
+     */
+    protected $table = 'text_pages';
+
     /**
      * @var array
      */
-    protected $fillable = [ 'html' ];
-
-    public function prepareFieldSet( FieldSet $fieldSet )
-    {
-        $fieldSet->add( new Richtext( 'html' ) );
-    }
-
-    /**
-     * @return Element
-     */
-    public function getHtmlAttribute()
-    {
-        // TODO: replace HTML placeholders - links, images, embed, etc.
-
-        return $this->getAttributeFromArray( 'html' ) ;
-
-//        return ( new Richtext( 'html' ) )->setValue( $this->getAttributeFromArray( 'html' ) );
-    }
+    protected $fillable = [
+        'html'
+    ];
 }
