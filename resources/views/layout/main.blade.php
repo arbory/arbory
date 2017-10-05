f095fc0ca399ea58e758721155d2c41e8011f0d8
Janis Osenieks <janis.osenieks@cube.lv>
2017-10-05 12:45:47 +0300

    modify admin view paths for new mix-manifest location

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,11 +4,11 @@
         <title>Arbory</title>
         <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <link href="{{ mix('/arbory/css/application.css') }}" media="all" rel="stylesheet"/>
-        <link href="{{ mix('/arbory/css/controllers/nodes.css') }}" media="all" rel="stylesheet"/>
+        <link href="{{ mix('/css/application.css', "arbory") }}" media="all" rel="stylesheet"/>
+        <link href="{{ mix('/css/controllers/nodes.css', "arbory") }}" media="all" rel="stylesheet"/>
 
         @foreach($assetsCss as $asset)
-            <link href="{{ mix( $asset ) }}" media="all" rel="stylesheet"/>
+            <link href="{{ mix( $asset, "arbory" ) }}" media="all" rel="stylesheet"/>
         @endforeach
 
         @if($inlineCss)
@@ -29,16 +29,16 @@
 
         <div class="notifications" data-close-text="Close"></div>
 
-        <script src="{{ mix('/arbory/js/application.js') }}"></script>
-        <script src="{{ mix('/arbory/js/controllers/nodes.js') }}"></script>
-        <script src="{{ mix('/arbory/js/admin.js') }}"></script>
+        <script src="{{ mix('/js/application.js', "arbory") }}"></script>
+        <script src="{{ mix('/js/controllers/nodes.js', "arbory") }}"></script>
+        <script src="{{ mix('/js/admin.js', "arbory") }}"></script>
 
         <script async defer
                 src="https://maps.googleapis.com/maps/api/js?key={{ env('GOOGLE_MAPS_API_KEY') }}">
         </script>
 
         @foreach($assetsJs as $asset)
-            <script src="{{ mix( $asset ) }}"></script>
+            <script src="{{ mix( $asset, "arbory" ) }}"></script>
         @endforeach
 
         @if($inlineJs)
