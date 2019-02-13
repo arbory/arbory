1ad48236f5127ceb9cc548dd7c852b559b2bb2b7
Jānis Ošenieks <janis.osenieks@cube.lv>
2019-02-13 19:07:15 +0200

    Modify settings primary key migration to work with sqlsrv

--- database/migrations/2017_06_07_113300_modify_primary_key_in_settings_table.php
+++ database/migrations/2017_06_07_113300_modify_primary_key_in_settings_table.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class ModifyPrimaryKeyInSettingsTable extends Migration
 {
@@ -10,10 +11,36 @@ class ModifyPrimaryKeyInSettingsTable extends Migration
      */
     public function up()
     {
-        Schema::table( 'settings', function ( Blueprint $table )
-        {
-            $table->dropColumn( 'id' );
-            $table->string( 'name' )->primary()->change();
-        } );
+        if (Schema::getConnection()->getDriverName() === 'sqlsrv') {
+            $this->dropPrimaryForSqlServer();
+        }
+
+        Schema::table('settings', function (Blueprint $table) {
+            $table->dropColumn('id');
+            $table->string('name')->primary()->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('settings', function (Blueprint $table) {
+            $table->dropPrimary('settings_name_primary');
+        });
+        Schema::table('settings', function (Blueprint $table) {
+            $table->increments('id');
+        });
+    }
+
+    protected function dropPrimaryForSqlServer()
+    {
+        $schema = Schema::getConnection()->getDoctrineSchemaManager();
+        $indexes = $schema->listTableIndexes('settings');
+        $primaryIndex = array_get($indexes, 'primary');
+
+        if ($primaryIndex) {
+            Schema::table('settings', function (Blueprint $table) use ($primaryIndex) {
+                $table->dropPrimary($primaryIndex->getName());
+            });
+        }
     }
 }
