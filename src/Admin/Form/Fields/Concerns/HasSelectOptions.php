9f0e93165d4b7d7a555f3d87a82107367332c884
Gints <gints.berzins@cubesystems.lv>
2019-02-26 17:35:14 +0200

    Automatically prepend select type fields with empty values, fix selects usage for relations

---
+++ src/Admin/Form/Fields/Concerns/HasSelectOptions.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Arbory\Base\Admin\Form\Fields\Concerns;
+
+use Illuminate\Support\Collection;
+
+/**
+ * Class HasSelectOptions
+ * @package Arbory\Base\Admin\Form\Fields\Concerns
+ */
+trait HasSelectOptions
+{
+    /** @var Collection $options */
+    protected $options;
+
+    /**
+     * @param Collection|array $options
+     * @return $this
+     */
+    public function options($options)
+    {
+        if (is_array($options)) {
+            $options = new Collection($options);
+        }
+
+        $this->options = $options;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection
+     */
+    public function getOptions(): Collection
+    {
+        return $this->options;
+    }
+}
