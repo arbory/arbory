70d690e4778873a289ea630b96795e5f47ef7f59
Marks Bogdanovs <git@ezitis.co>
2019-03-07 17:50:42 +0200

    Add animations to filter window
    Close on closeButton click, collapse filter

--- resources/assets/js/include/filter.js
+++ resources/assets/js/include/filter.js
@@ -1,12 +1,30 @@
 jQuery(document).ready(function()
 {
     let filterButton = $('.button.filter.trigger'),
+        filterCloseButton = $('.button.close'),
         filterWindow = $('.form-filter'),
-        contentWindow = $('#main section');
+        contentWindow = $('#main section'),
+        filterField = $('.accordion__heading');
 
     filterButton.on( 'click', function ()
     {
         filterWindow.toggleClass( 'show' );
         contentWindow.toggleClass( 'show-filter' );
     } );
+
+    filterCloseButton.on( 'click', function ()
+    {
+        filterWindow.removeClass( 'show' );
+        contentWindow.removeClass( 'show-filter' );
+    } );
+
+    filterField.on( 'click', function ( event ) {
+        let accordion = $( event.target ).closest( '.accordion' ),
+            accordionContent = $( accordion ).children( '.accordion__body' ),
+            accordionToggle = $( accordion ).find( '.button i' );
+
+        accordionContent.slideToggle( 150 );
+        accordionToggle.toggleClass('fa-minus');
+        accordionToggle.toggleClass('fa-plus');
+    });
 });
