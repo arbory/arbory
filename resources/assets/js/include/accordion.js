c80b279bf53e9d482dbfbec798f85f95364a3228
Marks Bogdanovs <git@ezitis.co>
2019-04-04 15:57:52 +0300

    Fix filtering on accordion close

--- resources/assets/js/include/accordion.js
+++ resources/assets/js/include/accordion.js
@@ -2,6 +2,8 @@ jQuery(document).ready(function()
 {
     $(".accordion .heading").on( "click", function(event)
     {
+        event.preventDefault();
+        
         var accordion = $( event.target ).closest(".accordion"),
             accordionContent = $( accordion ).children(".body"),
             accordionToggle = $( accordion ).find(".button i");
