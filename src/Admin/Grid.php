6018428401e39db15e3906cf97e7044db4684b66
Marks Bogdanovs <git@ezitis.co>
2019-04-04 16:18:11 +0300

    Turn off filter by default

--- src/Admin/Grid.php
+++ src/Admin/Grid.php
@@ -44,7 +44,7 @@ class Grid
     /**
      * @var array
      */
-    protected $enabledDefaultTools = [ 'create', 'search', 'filter' ];
+    protected $enabledDefaultTools = [ 'create', 'search' ];
 
     /**
      * @var array
