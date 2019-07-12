f582f1123d2b7d2bf2e1b259b7ed17f91194a99c
Dāvis Vagris <davis.vagris@cubesystems.lv>
2019-07-12 14:54:45 +0300

    Code style

--- src/Http/Middleware/ArboryRouteRedirectMiddleware.php
+++ src/Http/Middleware/ArboryRouteRedirectMiddleware.php
@@ -23,7 +23,7 @@ class ArboryRouteRedirectMiddleware
         $redirect->whereIn('from_url', [$request->url(), $request->path()]);
         $redirect->orWhere('from_url', 'LIKE', '_'.$request->path().'_');
         
-        $redirect = $redirect->first( [ 'to_url' ] );
+        $redirect = $redirect->first(['to_url']);
 
         if ($redirect) {
             return \Redirect::to($redirect->to_url, 301);
