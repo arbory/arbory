68a53e00dec5e172c08b73e830039c2cd3d159cf
Jānis Ošenieks <janis.osenieks@cube.lv>
2019-10-14 16:58:33 +0300

    Get absolute asset urls

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,8 +4,8 @@
         <title>Arbory</title>
         <meta content="text/html; charset=utf-8" http-equiv="Content-Type"/>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-        <link href="{{ mix('/css/application.css', 'arbory') }}" media="all" rel="stylesheet"/>
-        <link href="{{ mix('/css/controllers/nodes.css', 'arbory') }}" media="all" rel="stylesheet"/>
+        <link href="{{ asset(mix('/css/application.css', 'arbory')) }}" media="all" rel="stylesheet"/>
+        <link href="{{ asset(mix('/css/controllers/nodes.css', 'arbory')) }}" media="all" rel="stylesheet"/>
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -33,11 +33,11 @@
 
         <script src="https://maps.googleapis.com/maps/api/js?key={{ config('arbory.services.google.maps_api_key') }}&libraries=places"></script>
 
-        <script src="{{ mix('js/application.js', 'arbory') }}"></script>
-        <script src="{{ mix('js/controllers/nodes.js', 'arbory') }}"></script>
+        <script src="{{ asset(mix('js/application.js', 'arbory')) }}"></script>
+        <script src="{{ asset(mix('js/controllers/nodes.js', 'arbory')) }}"></script>
 
         @foreach($assets->getJs() as $script)
-            <script src="{{ mix($script, 'arbory') }}"></script>
+            <script src="{{ asset(mix($script, 'arbory')) }}"></script>
         @endforeach
 
         @foreach($assets->getInlineJs() as $inlineJs)
