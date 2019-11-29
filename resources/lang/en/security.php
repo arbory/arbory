52238fb8b082425303c84f2c29590f92418682c5
ausma_k <sausmakalnina@gmail.com>
2019-11-29 12:20:45 +0200

    Login page redesign

--- resources/lang/en/security.php
+++ resources/lang/en/security.php
@@ -6,4 +6,6 @@ return [
     'remember' => 'Remember me',
     'submit' => 'Sign in',
     'authentication_failed' => 'Please check your credentials and try again.',
+    'lost_password' => 'Lost your username or password?',
+    'contact_admin' => 'Please contact your administrator to recover your login information.'
 ];
--- resources/views/controllers/security/login.blade.php
+++ resources/views/controllers/security/login.blade.php
@@ -4,22 +4,29 @@
     <form class="login" action="{{route('admin.login.attempt')}}" accept-charset="UTF-8" method="post">
         {!!csrf_field()!!}
         <div class="field @if($errors->has('user.email'))has-error @endif">
-            <label for="email">{{ trans('arbory::security.email') }}</label>
             <input autofocus="autofocus" id="email" class="text" type="email"
-                   value="{{ $input->old('user.email') }}" name="user[email]">
+                   value="{{ $input->old('user.email') }}" name="user[email]" placeholder="{{ trans('arbory::security.email') }}">
         </div>
         <div class="field @if($errors->has('user.password'))has-error @endif">
-            <label for="password">{{ trans('arbory::security.password') }}</label>
-            <input id="password" class="text" type="password" name="user[password]">
+            <input id="password" class="text" type="password" name="user[password]" placeholder="{{ trans('arbory::security.password') }}">
         </div>
-        <div class="field">
-            <label>
-                <input type="checkbox" name="remember" value="1" {{ $input->old('remember') ? 'checked' : '' }} />
-                {{ trans('arbory::security.remember') }}
-            </label>
+        <div class="field type-checkbox">
+            <div class="value">
+                <label>
+                    <input type="checkbox" name="remember" value="1" {{ $input->old('remember') ? 'checked' : '' }} />
+                    <span></span>
+                    {{ trans('arbory::security.remember') }}
+                </label>
+            </div>
         </div>
         <div class="field">
             <button class="button" type="submit">{{ trans('arbory::security.submit') }}</button>
+        </div>
+
+
+        <div class="field info">
+            <p><b>{{ trans('arbory::security.lost_password') }}</b></p>
+            <p>{{ trans('arbory::security.contact_admin') }}</p>
         </div>
     </form>
 @endsection
