c19fe34f3aabc9bff76d024bd36a39fce8e6247e
Gints <gints.berzins@cubesystems.lv>
2019-12-19 16:57:45 +0200

    Apply uri prefix to node clone path

--- resources/assets/js/environment.js
+++ resources/assets/js/environment.js
@@ -1,1 +1,2 @@
 window.CKEDITOR_BASEPATH = '/' + [window.APP_URI_PREFIX, 'arbory/ckeditor/'].filter(String).join('/');
+window.NODE_CLONE_BASEPATH = '/' + [window.APP_URI_PREFIX, 'admin/nodes/api/node_reposition'].filter(String).join('/');
