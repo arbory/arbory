d3a768a7442b57da4e3dddb6b7061e8ec2596f89
andris <andris.gangis@cubesystems.lv>
2020-01-27 13:35:42 +0200

    Add dist folder

---
+++ dist/js/controllers/sessions.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{0:function(n,i,o){o("8mvK"),o("yi7R"),n.exports=o("iT2E")},"8mvK":function(n,i){},iT2E:function(n,i){},yi7R:function(n,i){}},[[0,0,1]]]);
