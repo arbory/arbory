01864c09744efb70fcff8a51efd063a67ec896b0
miks <miks@cubesystems.lv>
2021-04-07 13:03:42 +0300

    Fix laravel file manager support

--- config/lfm.php
+++ config/lfm.php
@@ -1,5 +1,13 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Documentation for this config :
+|--------------------------------------------------------------------------
+| online  => http://unisharp.github.io/laravel-filemanager/config
+| offline => vendor/unisharp/laravel-filemanager/docs/config.md
+ */
+
 return [
     /*
     |--------------------------------------------------------------------------
@@ -7,114 +15,109 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    // Include to pre-defined routes from package or not. Middlewares
-    'use_package_routes' => true,
-
-    // Middlewares which should be applied to all package routes.
-    // For laravel 5.1 and before, remove 'web' from the array.
-    'middlewares' => ['admin'],
-
-    // The url to this package. Change it if necessary.
-    'prefix' => '/admin/filemanager',
+    'use_package_routes' => false,
 
     /*
     |--------------------------------------------------------------------------
-    | Multi-User Mode
+    | Shared folder / Private folder
     |--------------------------------------------------------------------------
+    |
+    | If both options are set to false, then shared folder will be activated.
+    |
     */
 
-    // If true, private folders will be created for each signed-in user.
-    'allow_multi_user' => true,
-    // If true, share folder will be created when allow_multi_user is true.
-    'allow_share_folder' => true,
+    'allow_private_folder'     => true,
 
-    // Flexibla way to customize client folders accessibility
+    // Flexible way to customize client folders accessibility
     // If you want to customize client folders, publish tag="lfm_handler"
-    // Then you can rewrite userField function in App\Handler\ConfigHander class
-    // And set 'user_field' to App\Handler\ConfigHander::class
+    // Then you can rewrite userField function in App\Handler\ConfigHandler class
+    // And set 'user_field' to App\Handler\ConfigHandler::class
     // Ex: The private folder of user will be named as the user id.
-    'user_field' => Arbory\Base\Services\FileManager\FileManagerConfigHandler::class,
+    'private_folder_name'      => Arbory\Base\Services\FileManager\FileManagerConfigHandler::class,
 
-    /*0
-    |--------------------------------------------------------------------------
-    | Working Directory
-    |--------------------------------------------------------------------------
-    */
-
-    // Which folder to store files in project, fill in 'public', 'resources', 'storage' and so on.
-    // You should create routes to serve images if it is not set to public.
-    'base_directory' => 'public/storage/uploads',
-
-    'images_folder_name' => 'photos',
-    'files_folder_name'  => 'files',
+    'allow_shared_folder'      => true,
 
     'shared_folder_name' => 'shares',
-    'thumb_folder_name'  => 'thumbs',
 
     /*
     |--------------------------------------------------------------------------
-    | Startup Views
+    | Folder Names
     |--------------------------------------------------------------------------
     */
 
-    // The default display type for items.
-    // Supported: "grid", "list"
-    'images_startup_view' => 'grid',
-    'files_startup_view' => 'list',
+    'folder_categories'        => [
+        'file'  => [
+            'folder_name'  => 'files',
+            'startup_view' => 'grid',
+            'max_size'     => 50000, // size in KB
+            'valid_mime'   => [
+                'image/jpeg',
+                'image/pjpeg',
+                'image/png',
+                'image/gif',
+                'image/svg+xml',
+            ],
+        ],
+        'image' => [
+            'folder_name'  => 'photos',
+            'startup_view' => 'list',
+            'max_size'     => 50000, // size in KB
+            'valid_mime'   => [
+                'image/jpeg',
+                'image/pjpeg',
+                'image/png',
+                'image/gif',
+                'image/svg+xml',
+                'application/pdf',
+                'text/plain',
+            ],
+        ],
+    ],
 
     /*
     |--------------------------------------------------------------------------
     | Upload / Validation
     |--------------------------------------------------------------------------
-    */
+     */
 
-    // If true, the uploaded file will be renamed to uniqid() + file extension.
-    'rename_file' => false,
+    'disk'                     => 'public',
 
-    // If rename_file set to false and this set to true, then non-alphanumeric characters in filename will be replaced.
-    'alphanumeric_filename' => false,
+    'rename_file'              => false,
 
-    // If true, non-alphanumeric folder name will be rejected.
-    'alphanumeric_directory' => false,
+    'alphanumeric_filename'    => false,
 
-    // If true, the uploading file's size will be verified for over than max_image_size/max_file_size.
-    'should_validate_size' => false,
+    'alphanumeric_directory'   => false,
 
-    'max_image_size' => 50000,
-    'max_file_size' => 50000,
+    'should_validate_size'     => false,
 
-    // If true, the uploading file's mime type will be valid in valid_image_mimetypes/valid_file_mimetypes.
-    'should_validate_mime' => true,
+    'should_validate_mime'     => false,
 
-    // available since v1.3.0
-    'valid_image_mimetypes' => [
-        'image/jpeg',
-        'image/pjpeg',
-        'image/png',
-        'image/gif',
-        'image/svg+xml',
-    ],
-
-    // available since v1.3.0
-    // only when '/laravel-filemanager?type=Files'
-    'valid_file_mimetypes' => [
-        'image/jpeg',
-        'image/pjpeg',
-        'image/png',
-        'image/gif',
-        'image/svg+xml',
-        'application/pdf',
-        'text/plain',
-    ],
+    // behavior on files with identical name
+    // setting it to true cause old file replace with new one
+    // setting it to false show `error-file-exist` error and stop upload
+    'over_write_on_duplicate'  => false,
 
     /*
     |--------------------------------------------------------------------------
-    | Image / Folder Setting
+    | Thumbnail
     |--------------------------------------------------------------------------
-    */
+     */
 
-    'thumb_img_width' => 200,
-    'thumb_img_height' => 200,
+    // If true, image thumbnails would be created during upload
+    'should_create_thumbnails' => true,
+
+    'thumb_folder_name'        => 'thumbs',
+
+    // Create thumbnails automatically only for listed types.
+    'raster_mimetypes'         => [
+        'image/jpeg',
+        'image/pjpeg',
+        'image/png',
+    ],
+
+    'thumb_img_width'          => 200, // px
+
+    'thumb_img_height'         => 200, // px
 
     /*
     |--------------------------------------------------------------------------
@@ -135,21 +138,6 @@ return [
         'png'  => 'PNG Image',
         'ppt'  => 'Microsoft PowerPoint',
         'pptx' => 'Microsoft PowerPoint',
-    ],
-
-    'file_icon_array' => [
-        'pdf'  => 'fa-file-pdf-o',
-        'doc'  => 'fa-file-word-o',
-        'docx' => 'fa-file-word-o',
-        'xls'  => 'fa-file-excel-o',
-        'xlsx' => 'fa-file-excel-o',
-        'zip'  => 'fa-file-archive-o',
-        'gif'  => 'fa-file-image-o',
-        'jpg'  => 'fa-file-image-o',
-        'jpeg' => 'fa-file-image-o',
-        'png'  => 'fa-file-image-o',
-        'ppt'  => 'fa-file-powerpoint-o',
-        'pptx' => 'fa-file-powerpoint-o',
     ],
 
     /*
