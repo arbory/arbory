f223b1b44e09431945dbee363c49c3228513c91f
miks <miks@cubesystems.lv>
2024-08-29 16:02:31 +0300

    Bump node packages, rebuild assets

--- dist/ckeditor/plugins/devtools/lang/he.js
+++ dist/ckeditor/plugins/devtools/lang/he.js
@@ -1,5 +1,5 @@
 ﻿/*
- Copyright (c) 2003-2022, CKSource Holding sp. z o.o. All rights reserved.
- For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ Copyright (c) 2003-2024, CKSource Holding sp. z o.o. All rights reserved.
+ CKEditor 4 LTS ("Long Term Support") is available under the terms of the Extended Support Model.
 */
 CKEDITOR.plugins.setLang("devtools","he",{title:"מידע על האלמנט",dialogName:"שם הדיאלוג",tabName:"שם הטאב",elementId:"ID של האלמנט",elementType:"סוג האלמנט"});
