d1955ab899f2610e19e1a7e4a3c3f9a02aaa3ba4
Dmitrijs Mihailovs <dmitrijs.mihailovs@cube.lv>
2025-06-12 11:18:16 +0300

    Update 2FA service provider

--- src/Providers/TwoFactorServiceProvider.php
+++ src/Providers/TwoFactorServiceProvider.php
@@ -5,6 +5,7 @@ namespace Arbory\Base\Providers;
 use Arbory\Base\Rules\Totp;
 use Arbory\Base\Services\Authentication\Helpers\TwoFactorAuth;
 use Illuminate\Foundation\Application;
+use Laragear\TwoFactor\Console\TwoFactorInstallCommand;
 use Laragear\TwoFactor\Http\Middleware\ConfirmTwoFactorCode;
 use Laragear\TwoFactor\Http\Middleware\RequireTwoFactorEnabled;
 
@@ -20,6 +21,8 @@ class TwoFactorServiceProvider extends \Laragear\TwoFactor\TwoFactorServiceProvi
     public function register(): void
     {
         $this->mergeConfigFrom(static::CONFIG, 'two-factor');
+
+        $this->commands(TwoFactorInstallCommand::class);
 
         $this->app->bind(TwoFactorAuth::class, static function (Application $app): TwoFactorAuth {
             $config = $app->make('config');
@@ -39,7 +42,6 @@ class TwoFactorServiceProvider extends \Laragear\TwoFactor\TwoFactorServiceProvi
     {
         $this->loadViewsFrom(static::VIEWS, 'two-factor');
         $this->loadTranslationsFrom(static::LANG, 'two-factor');
-        $this->loadMigrationsFrom(static::MIGRATIONS);
 
         $this->withMiddleware(RequireTwoFactorEnabled::class)->as('2fa.enabled');
         $this->withMiddleware(ConfirmTwoFactorCode::class)->as('2fa.confirm');
@@ -58,6 +60,8 @@ class TwoFactorServiceProvider extends \Laragear\TwoFactor\TwoFactorServiceProvi
      */
     protected function publishFiles(): void
     {
+        $this->publishMigrations();
+
         $this->publishes([static::CONFIG => $this->app->configPath('two-factor.php')], 'config');
     }
 }
